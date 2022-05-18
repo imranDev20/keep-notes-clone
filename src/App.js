2932239af7488769ed96f6ef8edf4dfc46f1534a
imrandev20 <imrandev20@gmail.com>
2022-05-19 02:35:58 +0600

    forgot to add .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+<TextField id="outlined-basic" label="Outlined" variant="outlined" />;
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasksArray, setTasksArray] = useState([
+    { id: 1, value: "Buy Books" },
+    { id: 2, value: "Develop" },
+  ]);
+
+  console.log(task);
+
+  const addToTasks = () => {
+    console.log(task);
+    const newTask = { id: tasksArray.length + 1, value: task };
+    const newTaskList = [...tasksArray, newTask];
+    setTasksArray(newTaskList);
+  };
+
+  const deleteTask = (id) => {
+    const newArray = tasksArray.filter((item) => item.id !== id);
+    setTasksArray(newArray);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          type="text"
+          name="task"
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Please write down your task"
+        />
+        <button onClick={addToTasks}>Add</button>
+      </div>
+      <div>
+        {tasksArray.map((item) => {
+          return (
+            <p key={item.id}>
+              {item.value}{" "}
+              <button onClick={() => deleteTask(item.id)}>Delete</button>
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
 }
